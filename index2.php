19c65d1dbe4e546c837319fdf8cf35989ef49f26
miaojuanfeng <miaojuanfeng@qq.com>
2017-09-14 16:53:15 +0800

    update text color and format

--- index2.php
+++ index2.php
@@ -19,7 +19,7 @@
 	}
 	$lines2 = $file_arr;
 
-	$diff = new Diff($lines1, $lines2);
+	$line_diff = new Diff($lines1, $lines2);
 
 // echo "<pre>";
 // 	var_dump($diff);die();
@@ -27,7 +27,7 @@
 	$origin = array();
 	$change = array();
 
-	foreach( $diff->edits as $line_num => $line ){
+	foreach( $line_diff->edits as $line_num => $line ){
 		if( $line->type == 'copy' ) {
 			foreach ($line->orig as $key => $value) {
 				$origin[] = '<p class="diff-chunk"><span class="diff-chunk diff-chunk-equal">'.$value.'</span></p>';
@@ -49,82 +49,229 @@
 				}
 			}
 		}else if( $line->type == 'change' ){
-			foreach ($line->orig as $key => $value) {
-				$orig_letter = array();
-				$closing_letter = array();
-
-				if( isset($line->orig[$key]) ){
-					$pscws->send_text($line->orig[$key]);
-					while ($some = $pscws->get_result()){
-					   foreach ($some as $letter){
-					      $orig_letter[] = $letter['word'];
-					   }
+			$word_diff = new WordLevelDiff($line->orig, $line->closing);
+			$origin_word_line = '<p class="diff-chunk diff-line-with-removes">';
+			$change_word_line = '<p class="diff-chunk diff-line-with-inserts">';
+			foreach ($word_diff->edits as $word_num => $word) {
+				if( $word->type == 'copy' ) {
+					$origin_word_line .= '<span class="diff-chunk diff-chunk-equal">';
+					foreach ($word->orig as $key => $value) {
+						$origin_word_line .= $value;
 					}
-				}
-
-				if( isset($line->closing[$key]) ){
-					$pscws->send_text($line->closing[$key]);
-					while ($some = $pscws->get_result()){
-					   foreach ($some as $letter){
-					      $closing_letter[] = $letter['word'];
-					   }
+					$origin_word_line .= '</span>';
+					$change_word_line .= '<span class="diff-chunk diff-chunk-equal">';
+					foreach ($word->closing as $key => $value) {
+						$change_word_line .= $value;
 					}
-				}
+					$change_word_line .= '</span>';
+				}else if( $word->type == 'change' ){
+					$origin_letter_line = implode('', $word->orig);
+					$change_letter_line = implode('', $word->closing);
 
-				if( $value == "\n" ){
-					$origin[] = '<p class="diff-chunk diff-line-empty"></p>';
-					if( count($line->orig) > count($line->closing) ){
-						$change[] = '<p class="diff-chunk diff-line-with-removes"></p>';
-					}
-				}else{
-					foreach ($orig_letter as $k => $v) {
-						if( (isset($closing_letter[$k]) && $orig_letter[$k] !== $closing_letter[$k]) ||
-							!isset($closing_letter[$k])
-						){
-							$orig_letter[$k] = '<span class="diff-chunk diff-chunk-removed">'.$orig_letter[$k].'</span>';
+					$orig_letter = array();
+					$closing_letter = array();
+
+					// var_dump($word);
+					// var_dump($origin_letter_line);
+					// var_dump($change_letter_line);
+
+					foreach ($word->orig as $key => $value) {
+						if( isset($word->orig[$key]) ){
+							$pscws->send_text($word->orig[$key]);
+							while ($some = $pscws->get_result()){
+							   foreach ($some as $letter){
+							      $orig_letter[] = $letter['word'];
+							   }
+							}
 						}
 					}
-					$origin[] = '<p class="diff-chunk diff-line-with-removes">'.implode('', $orig_letter).'</p>';
-				}
-			}
-			foreach ($line->closing as $key => $value) {
-				$orig_letter = array();
-				$closing_letter = array();
 
-				if( isset($line->orig[$key]) ){
-					$pscws->send_text($line->orig[$key]);
-					while ($some = $pscws->get_result()){
-					   foreach ($some as $letter){
-					      $orig_letter[] = $letter['word'];
-					   }
-					}
-				}
-
-				if( isset($line->closing[$key]) ){
-					$pscws->send_text($line->closing[$key]);
-					while ($some = $pscws->get_result()){
-					   foreach ($some as $letter){
-					      $closing_letter[] = $letter['word'];
-					   }
-					}
-				}
-
-				if( $value == "\n" ){
-					$change[] = '<p class="diff-chunk diff-line-empty"></p>';
-					if( count($line->orig) < count($line->closing) ){
-						$origin[] = '<p class="diff-chunk diff-line-with-removes"></p>';
-					}
-				}else{
-					foreach ($closing_letter as $k => $v) {
-						if( (isset($orig_letter[$k]) && $closing_letter[$k] !== $orig_letter[$k]) ||
-							!isset($orig_letter[$k])
-						){
-							$closing_letter[$k] = '<span class="diff-chunk diff-chunk-inserted">'.$closing_letter[$k].'</span>';
+					foreach ($word->closing as $key => $value) {
+						if( isset($word->closing[$key]) ){
+							$pscws->send_text($word->closing[$key]);
+							while ($some = $pscws->get_result()){
+							   foreach ($some as $letter){
+							      $closing_letter[] = $letter['word'];
+							   }
+							}
 						}
 					}
-					$change[] = '<p class="diff-chunk diff-line-with-inserts">'.implode('', $closing_letter).'</p>';
+
+					// var_dump($orig_letter);
+					// var_dump($closing_letter);
+
+					$letter_diff = new WordLevelDiff($orig_letter, $closing_letter);
+					foreach ($letter_diff->edits as $letter_num => $letter) {
+						// var_dump($letter);
+						if( $letter->type == 'copy' ) {
+							// $origin_word_line .= '<span class="diff-chunk diff-chunk-equal">';
+							// foreach ($letter->orig as $key => $value) {
+							// 	// 分词后会产生\n换行符，因为在同一行，无需换行，去掉此字符
+							// 	if( $value !== "\n" ){
+							// 		$origin_word_line .= $value;
+							// 	}
+							// }
+							// $origin_word_line .= '</span>';
+							// $change_word_line .= '<span class="diff-chunk diff-chunk-equal">';
+							// foreach ($letter->closing as $key => $value) {
+							// 	// 分词后会产生\n换行符，因为在同一行，无需换行，去掉此字符
+							// 	if( $value !== "\n" ){
+							// 		$change_word_line .= $value;
+							// 	}
+							// }
+							// $change_word_line .= '</span>';
+						}else if( $letter->type == 'change' ){
+							//$origin_word_line .= '<span class="diff-chunk diff-chunk-removed">';
+							foreach ($letter->orig as $key => $value) {
+								// 分词后会产生\n换行符，因为在同一行，无需换行，去掉此字符
+								if( $value !== "\n" ){
+									// 会产生替换多次的问题
+									$origin_letter_line = str_replace($value, '<span class="diff-chunk diff-chunk-changed">'.$value.'</span>', $origin_letter_line);
+								}
+							}
+							//$origin_word_line .= '</span>';
+							//$change_word_line .= '<span class="diff-chunk diff-chunk-removed">';
+							foreach ($letter->closing as $key => $value) {
+								// 分词后会产生\n换行符，因为在同一行，无需换行，去掉此字符
+								if( $value !== "\n" ){
+									$change_letter_line = str_replace($value, '<span class="diff-chunk diff-chunk-inserted">'.$value.'</span>', $change_letter_line);
+								}
+							}
+							//$change_word_line .= '</span>';
+						}else if( $letter->type == 'delete' ){
+							if( $letter->orig ){
+								//$origin_word_line .= '<span class="diff-chunk diff-chunk-removed">';
+								foreach ($letter->orig as $key => $value) {
+									// 分词后会产生\n换行符，因为在同一行，无需换行，去掉此字符
+									if( $value !== "\n" ){
+										// 会产生替换多次的问题
+										$origin_letter_line = str_replace($value, '<span class="diff-chunk diff-chunk-removed">'.$value.'</span>', $origin_letter_line);
+									}
+								}
+								//$origin_word_line .= '</span>';
+							}
+							if( $letter->closing ){
+								//$change_word_line .= '<span class="diff-chunk diff-chunk-removed">';
+								foreach ($letter->closing as $key => $value) {
+									// 分词后会产生\n换行符，因为在同一行，无需换行，去掉此字符
+									if( $value !== "\n" ){
+										$change_letter_line = str_replace($value, '<span class="diff-chunk diff-chunk-inserted">'.$value.'</span>', $change_letter_line);
+									}
+								}
+								//$change_word_line .= '</span>';
+							}
+						}else{
+							echo "undefined letter: ";
+							var_dump($letter);
+						}
+					}
+					$origin_word_line .= $origin_letter_line;
+					$change_word_line .= $change_letter_line;
 				}
 			}
+			$origin_word_line .= '</p>';
+			$change_word_line .= '</p>';
+			$origin[] = $origin_word_line;
+			$change[] = $change_word_line;
+			// foreach ($origin as $key => $value) {
+			// 	echo $value;
+			// };
+			// die();
+
+
+
+			// foreach ($line->orig as $key => $value) {
+			// 	$orig_letter = array();
+			// 	$closing_letter = array();
+
+			// 	if( isset($line->orig[$key]) ){
+			// 		$pscws->send_text($line->orig[$key]);
+			// 		while ($some = $pscws->get_result()){
+			// 		   foreach ($some as $letter){
+			// 		      $orig_letter[] = $letter['word'];
+			// 		   }
+			// 		}
+			// 	}
+
+			// 	if( isset($line->closing[$key]) ){
+			// 		$pscws->send_text($line->closing[$key]);
+			// 		while ($some = $pscws->get_result()){
+			// 		   foreach ($some as $letter){
+			// 		      $closing_letter[] = $letter['word'];
+			// 		   }
+			// 		}
+			// 	}
+
+			// 	die();
+
+			// 	if( $value == "\n" ){
+			// 		$origin[] = '<p class="diff-chunk diff-line-empty"></p>';
+			// 		if( count($line->orig) > count($line->closing) ){
+			// 			$change[] = '<p class="diff-chunk diff-line-with-removes"></p>';
+			// 		}
+			// 	}else{
+			// 		foreach ($orig_letter as $k => $v) {
+			// 			if( (isset($closing_letter[$k]) && $orig_letter[$k] !== $closing_letter[$k]) ||
+			// 				!isset($closing_letter[$k])
+			// 			){
+			// 				// $count = substr_count($value, $orig_letter[$k]);
+			// 				// $j = 0;
+			// 				// for($i = 0; $i < $count; $i++){
+			// 				//      $j = strpos($value, $orig_letter[$k], $j);
+			// 				//      $value = substr_replace($value, '<span class="diff-chunk diff-chunk-removed">'.$orig_letter[$k].'</span>', $j, strlen($orig_letter[$k]));
+			// 				//      $j += 51;
+			// 				//      $j = $j+1;
+			// 				// }
+			// 				// var_dump($orig_letter[$k]);
+			// 				// var_dump($closing_letter[$k]);
+			// 				// echo "<br>";
+			// 				$orig_letter[$k] = '<span class="diff-chunk diff-chunk-removed">'.$orig_letter[$k].'</span>';
+			// 			}
+			// 		}
+			// 		// echo "<br/>";
+			// 		// var_dump($value);die();
+			// 		$origin[] = '<p class="diff-chunk diff-line-with-removes">'.implode('', $orig_letter).'</p>';
+			// 		// $origin[] = '<p class="diff-chunk diff-line-with-removes">'.$value.'</p>';
+			// 	}
+			// }
+			// foreach ($line->closing as $key => $value) {
+			// 	$orig_letter = array();
+			// 	$closing_letter = array();
+
+			// 	if( isset($line->orig[$key]) ){
+			// 		$pscws->send_text($line->orig[$key]);
+			// 		while ($some = $pscws->get_result()){
+			// 		   foreach ($some as $letter){
+			// 		      $orig_letter[] = $letter['word'];
+			// 		   }
+			// 		}
+			// 	}
+
+			// 	if( isset($line->closing[$key]) ){
+			// 		$pscws->send_text($line->closing[$key]);
+			// 		while ($some = $pscws->get_result()){
+			// 		   foreach ($some as $letter){
+			// 		      $closing_letter[] = $letter['word'];
+			// 		   }
+			// 		}
+			// 	}
+
+			// 	if( $value == "\n" ){
+			// 		$change[] = '<p class="diff-chunk diff-line-empty"></p>';
+			// 		if( count($line->orig) < count($line->closing) ){
+			// 			$origin[] = '<p class="diff-chunk diff-line-with-removes"></p>';
+			// 		}
+			// 	}else{
+			// 		foreach ($closing_letter as $k => $v) {
+			// 			if( (isset($orig_letter[$k]) && $closing_letter[$k] !== $orig_letter[$k]) ||
+			// 				!isset($orig_letter[$k])
+			// 			){
+			// 				$closing_letter[$k] = '<span class="diff-chunk diff-chunk-inserted">'.$closing_letter[$k].'</span>';
+			// 			}
+			// 		}
+			// 		$change[] = '<p class="diff-chunk diff-line-with-inserts">'.implode('', $closing_letter).'</p>';
+			// 	}
+			// }
 		}else if( $line->type == 'delete' ){
 			if( $line->orig ){
 				foreach ($line->orig as $key => $value) {
@@ -139,6 +286,7 @@
 				}
 			}
 		}else{
+			echo "undefined line: ";
 			var_dump($line);
 		}
 	}
@@ -176,17 +324,24 @@
 		font-family: Inconsolata,Consolas,Monaco;
 		white-space: pre-wrap;
 	}
-	.diff-line-with-removes{
-		background: #fcd8d9;;
-	}
-	.diff-chunk-removed{
-		background: #f88;
-	}
 	.diff-line-with-inserts{
 		background: #e0fcd0;
 	}
 	.diff-chunk-inserted{
 		background: #16ff00;
+	}
+	.diff-line-with-changes{
+		background: #e0fcd0;
+	}
+	.diff-chunk-changed{
+		background: #16ccff;
+	}
+	.diff-line-with-removes{
+		background: #fcd8d9;;
+	}
+	.diff-chunk-removed{
+		background: #f88;
+		text-decoration: line-through;
 	}
 	.diff-line-empty{
 		background: #ccc;
--- order1.php
+++ order1.php
@@ -1,32 +1,41 @@
-<?php
-/*
- * Last modified: 2016-12-01
- * Translate english <-> chinese
- * Agoda modified  + 
- * Axa coupon
- * 
- * */
-session_start();
+CII 是一个小巧但功能强大的 PHP 扩展框架，作为一个简单优雅的工具包，它可以为 PHP 程序员建立功能完善并且快速的 Web 应用程序。
 
-$sorder_country = $_GET['country'];
-$sorder_departuredate = $_GET['d_date'];
-$sdevice_name = $_GET['device_name'];
+如果你已经厌倦了那些傻大笨粗的框架，为 CodeIgniter的运行效率而烦恼，并且你的站点运行在独立主机中，那么 CII 扩展框架就是你所需要的。
 
-$promotion_id =$_GET['prtion_id'];
+特性
 
-if($promotion_id != '') $sorder_qty = 1;
-if($sorder_countrys != '' && $sorder_qty =='') {$sorder_qty = 1;}
-if($sorder_country == '' && $promotions_id =='') {$sorder_qty = '2';}
+ CII 高性能扩展 框架，扩展全称为 CodeIgniter Internal Framework 。
+ 这是根据目前非常流行的开源框架 CodeIgniter 的架构，以 PHP 扩展方式实现的框架。
+ 完全采用 C 语言编写而成，编译为动态链接库，在 PHP 启动时装载运行。
+ 采用 CII 框架开发网站的速度和便利不输于 CodeIgniter 。
+ 支持 Codeigniter 框架结构及语法。
+ 对比 Codeignter 拥有超过 5 倍的性能提升。
+ 对比 Codeignter 减少了 75% 的内存使用。
+ 经过线下单元测试与线上长期运行，CII 框架运行时不会产生段错误与内存泄露 。
+ 未来的代码优化，会带来更好的性能表现。
+运行环境
 
+CII 扩展在以下环境中开发测试及运行：
 
-if(!$is_login) header("Location: sigin.php");
-$is_fb_login =$_SESSION['utype'] == 'fb'? true: false;
+OS：
 
+ Mac OS X EI Capitan 10.11.6
+ Ubuntu Kylin 14.04 64bit
+ Windows 7 64bit
+PHP：
 
-if($is_login){
-$uname = $_SESSION['uname'];
-$uemail = $_SESSION['uemail'];
-$uid = $_SESSION['uid'];
-}
+ PHP-5.6.25
+Server:
 
-?>
+ Httpd 2.4.27
+Mysql：
+
+ 任何 Mysqli 扩展支持的版本
+PHP编译方式
+
+CII 框架支持 PHP 的单线程模式与多线程模式。如果 PHP 被编译成单线程，采用 CGI 方式与服务器协作。参考编译配置：
+
+--prefix=/web/php --disable-maintainer-zts --enable-debug --with-mysql=mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd
+如果 PHP 被编译成 Apache 模块，采用多线程方式与服务器协作，编译 PHP 时需要开启多线程模式。
+
+线上安装请关闭 PHP 的 Debug 模式，去除 --enable-debug
